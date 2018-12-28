95c48423e7f93e0b1e8616f21c93c6353f3ecc48
Sean Howard <seener49@gmail.com>
2018-12-28 14:46:17 -0500

    Cleaned up the variable naming conventions and made the for loop a little bit easier to follow in the get_style_sheet function.

--- demo_shop_version_checker.py
+++ demo_shop_version_checker.py
@@ -54,16 +54,19 @@ def get_style_sheet(html_head):
     -------
     result: string with the stylesheet information
     """
-    for comments in html_head.find_all(text=lambda text: isinstance(text, Comment)):
-        if "stylesheets for" in str.lower(comments.extract()):
-            return comments.extract().strip()
+    all_comments = html_head.find_all(text=lambda t: isinstance(t, Comment))
+    out = None
+    for comment in all_comments:
+        if "stylesheets for" in str.lower(comment.extract()):
+            out = comment.extract().strip()
+    return out
 
 
 FILE = input("Provide full path to the file with URLS: ")
 with open(FILE, 'r') as f:
     URLS = f.read().split("\n")
 
-for url in URLS:
-    HEAD = get_page(url).head  # get just he head of the page
+for link in URLS:
+    HEAD = get_page(link).head  # get just he head of the page
     STYLE = get_style_sheet(HEAD)  # get the style sheet used
-    print(url + ": " + STYLE)
+    print(link + ": " + STYLE)
